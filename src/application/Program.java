cf046fcb1c4fe4b91bfe38d7369815e61b75e381
Arthur <arthusantosfernandes337@gmail.com>
2026-05-25 20:05:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,15 +2,19 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
 
         Department department = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bog@gmail.com", new Date(), 3000.0, department);
         System.out.println(department);
+        System.out.println(seller);
 
 
 
